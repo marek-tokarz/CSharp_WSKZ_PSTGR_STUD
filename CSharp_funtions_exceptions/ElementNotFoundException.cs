e12eaa03dcb94a3fda13adeff9a86d7f10f65158
marek-tokarz <marek.tokarz14@gmail.com>
2025-03-29 15:34:04 +0100

    Simple exercises with user defined exceptions - try, catch, throw

---
+++ CSharp_funtions_exceptions/ElementNotFoundException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_funtions_exceptions
+{
+    public class ElementNotFoundException : ApplicationException
+    {
+        public ElementNotFoundException() : base("Element has not been found")
+        {
+
+        }
+
+        public ElementNotFoundException(String message) : base(message)
+        {
+
+        }
+    }
+}
--- CSharp_funtions_exceptions/Program.cs
+++ CSharp_funtions_exceptions/Program.cs
@@ -1,11 +1,12 @@
-﻿namespace FunctionsAndExceptions
+﻿using CSharp_funtions_exceptions;
+
+namespace FunctionsAndExceptions
 {
     public class Program
     {
         // Reversing array: [1, 2, 3, 4] -> [4, 3, 2, 1]
         //                   i        j
         //                      i  j
-
         private static double[] ReverseArray(double[] array)
         {
             int numberOfElements = array.Length;
@@ -23,14 +24,24 @@
                     j--;
                 }
             }
-
             return array;
         }
 
+        // Printing a result of array reversing
+        private static String CreateResultOfReverseArray(double[] array)
+        {
+            String result = "";
+            foreach (double val in array)
+            {
+                result += val.ToString() + " ";
+            }
+            return result;
+        }
+
+        // Summing an elements af an array if they are within a specified range
         private static double SumOfElements(double[] sum, double a, double b)
         {
             double result = 0;
-
             foreach(double val in sum)
             {
                 if(val >= a && val <= b)
@@ -38,21 +49,40 @@
                     result += val;
                 }
             }
-
             return result;
         }
 
-        // Printing a result array
-        private static String CreateResultOfReverseArray(double[] array)
+        // Functions looks for a specific word (string) in an array of strings
+        // ["abc", "xyz"], xyz -> 1
+        // ["
+        private static int FindWord(String[] words, String word)
         {
-            String result = "";
-            foreach( double val in array)
+            for(int i = 0; i < words.Length; i++)
             {
-                result += val.ToString() + " ";
+                if (words[i] == word)
+                {
+                    return i;
+                }
             }
-            return result;
+            throw new ElementNotFoundException();
         }
 
+        private static double MathFunction(double x, double y)
+        {
+            if(x < 0)
+            {
+                throw new ArgumentException("Wrong argument 'x', value should be positive");
+            }
+
+            if (y == 0)
+            {
+                throw new ArgumentException("Wrong argument 'y', value can not be equal to 0");
+            }
+
+            return Math.Sqrt(x * x) + 1 / y;
+
+        }
+ 
         public static void Main()
         {
             double[] test_array_1 = { 1, 2, 3, 4, 5 };
@@ -69,6 +99,40 @@
             Console.WriteLine("sum1: " + sum1);
             double sum2 = SumOfElements(test_array_1, 1, 3);
             Console.WriteLine("sum2: " + sum2);
+
+            try
+            {
+                Console.WriteLine(test_array_2[4]);
+            } catch(Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                // return;
+            } finally
+            {
+                // this section will be always executed - independently of an execption
+                Console.WriteLine("Cleaning...");
+            }
+
+            String[] words = { "abc", "xyz", "www" };
+            int index1 = FindWord(words, "www");
+            Console.WriteLine("index of a found word is: " + index1);
+
+            try
+            {
+                int index2 = FindWord(words, "bbb");
+                Console.WriteLine("index of a found word is: " + index2);
+            } catch(ElementNotFoundException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            try
+            {
+                MathFunction(4.0, 0);
+            } catch(ArgumentException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
         }
     }
 }
