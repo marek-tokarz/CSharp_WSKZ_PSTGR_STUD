e286665bac8e4d5ac0bc6058f2aa3d60b9485f98
marek-tokarz <marek.tokarz14@gmail.com>
2025-03-30 08:10:20 +0200

    Simple example with delegates: a multicast delegate

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -2,6 +2,20 @@
 {
     public class Program
     {
+        private static double globalResult = 0;
+
+        private delegate void TwoArgFun(double x, double y);
+
+        private static void sum(double a, double b)
+        {
+            globalResult += a + b;
+        }
+
+        private static void multiply(double a, double b)
+        {
+            globalResult += a * b;
+        }
+
         // Delegate is a reference type concerning a function
         private delegate string OneArgStringFunction(string value);
 
@@ -25,14 +39,28 @@
 
         public static void Main()
         {
-            OneArgStringFunction f = new OneArgStringFunction(UpLetters);
-            string result1 = StringOperation("aBac", f); 
+            // single-cast delegate
+
+            OneArgStringFunction function1 = new OneArgStringFunction(UpLetters);
+            string result1 = StringOperation("aBac", function1); 
 
             Console.WriteLine(result1); // BBBC
 
             string result2 = StringOperation("aBac", new OneArgStringFunction(DownLetters));
 
             Console.WriteLine(result2); // bbbc
+
+            // multicast delegate
+
+            TwoArgFun function2 = new TwoArgFun(sum);
+            TwoArgFun function3 = new TwoArgFun(multiply);
+
+            TwoArgFun functions = function2;
+            functions += function3;
+
+            functions(2, 3);
+
+            Console.WriteLine("globalResult: " + globalResult);
 
         }
     }
