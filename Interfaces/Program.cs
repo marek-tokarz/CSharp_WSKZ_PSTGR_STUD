1589e168cca6b0049529bf4e5f711c4392585662
marek-tokarz <marek.tokarz14@gmail.com>
2025-03-30 15:33:56 +0200

    Another example of interface - a file logger with two different implementations of a one interface (similar to the abstract class, but there are some differences)

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -25,8 +25,44 @@
         }
     }
 
+    public interface ILogger
+    {
+        void LogInfo(String message);
+        void LogError(String message);
+    }
+
+    public class FileLogger: ILogger
+    {
+        public void LogInfo(String message)
+        {
+            Console.WriteLine("FileLogger, Info, {0}, {1}", DateTime.Now.ToString("dddd, dd MMMM yyyy"), message);
+        }
+
+        public void LogError(String message)
+        {
+            Console.WriteLine("FileLogger, Error, {0}, {1}", DateTime.Now.ToString("dddd, dd MMMM yyyy"), message);
+        }
+    }
+
+    public class ConsoleLogger : ILogger
+    {
+        public void LogInfo(String message)
+        {
+            Console.WriteLine("ConsoleLogger, Info, {0}, {1}", DateTime.Now.ToString("dddd, dd MMMM yyyy"), message);
+        }
+
+        public void LogError(String message)
+        {
+            Console.WriteLine("ConsoleLogger, Error, {0}, {1}", DateTime.Now.ToString("dddd, dd MMMM yyyy"), message);
+        }
+    }
+
     public class Program
     {
+        private static ILogger logger;
+        private static ILogger logger_console;
+        
+
         private static void archData(IBackup backup)
         {
             Console.WriteLine("archData started");
@@ -34,10 +70,18 @@
         }
         public static void Main()
         {
+            logger = new FileLogger();
+
+            logger.LogInfo("application has been started");
+
             IBackup backup = new DbTableBackup();
             archData(backup);
             backup.defaultMethod();
             archData(new FileBackup());
+
+            logger_console = new ConsoleLogger();
+            logger_console.LogInfo("application is saving file to the harddrive");
+            logger_console.LogInfo("application has finished");
         }
     }
 }
