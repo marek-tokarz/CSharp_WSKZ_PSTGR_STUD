690d95f9b71a3ec8d091b20f81228c8b0d45ee5a
marek-tokarz <marek.tokarz14@gmail.com>
2025-03-30 07:57:28 +0200

    Simple example with delegates: a single-cast delegate

---
+++ DelegatesAndEvents/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace DelegatesAndEvents
+{
+    public class Program
+    {
+        // Delegate is a reference type concerning a function
+        private delegate string OneArgStringFunction(string value);
+
+        private static string StringOperation(string word, OneArgStringFunction fun)
+        {
+            string temp = word.Replace('a', 'B');
+
+            return fun(temp);
+
+        }
+
+        private static string UpLetters(string w)
+        {
+            return w.ToUpper();
+        }
+
+        private static string DownLetters(string w)
+        {
+            return w.ToLower();
+        }
+
+        public static void Main()
+        {
+            OneArgStringFunction f = new OneArgStringFunction(UpLetters);
+            string result1 = StringOperation("aBac", f); 
+
+            Console.WriteLine(result1); // BBBC
+
+            string result2 = StringOperation("aBac", new OneArgStringFunction(DownLetters));
+
+            Console.WriteLine(result2); // bbbc
+
+        }
+    }
+}
