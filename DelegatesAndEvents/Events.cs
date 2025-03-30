097060f5767e51725bb5697d417b296f42fc33fa
marek-tokarz <marek.tokarz14@gmail.com>
2025-03-30 10:11:47 +0200

    Minor changes in a file Events.cs

--- DelegatesAndEvents/Events.cs
+++ DelegatesAndEvents/Events.cs
@@ -4,7 +4,6 @@
     {
         public delegate void Notification();
         public event Notification migrationStarted;
-
         public void ExecuteDataMigration()
         {
             OnDataMigrationStarted();   // a notofication will be send before migration
@@ -13,17 +12,14 @@
 
             OnDataMigrationCompleted(); // another logic after finishing migration
         }
-
         private void OnDataMigrationStarted()
         {
             migrationStarted?.Invoke();
         }
-
         private void OnDataMigrationCompleted()
         {
 
         }
-
     }
     class Events
     {
@@ -42,9 +38,6 @@
             dataMigrator.migrationStarted += SendEmail;
             dataMigrator.migrationStarted += SendSlackNotification;
             dataMigrator.ExecuteDataMigration();
-
         }
-
-
     }
 }
