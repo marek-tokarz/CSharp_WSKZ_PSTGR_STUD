afed74efe3c4a4f54dfd9852db8457a64b010ea0
Marek Tokarz <marek.tokarz14@gmail.com>
2025-04-05 16:36:01 +0200

    Acces modifiers: protected, accesing a variables in a deriving class

---
+++ BaseFieldAcces/FunctionOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseFieldAcces
+{
+    internal class FunctionOperations
+    {
+        protected int aFactor = 0;
+        protected int bFactor = 0;
+        protected int cFactor = 0;
+
+        public void setA(int a) { this.aFactor = a; }
+
+        public void setB(int b) { this.bFactor = b; }
+
+        public void setC(int c) { this.cFactor = c; }
+    }
+}
---
+++ BaseFieldAcces/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseFieldAcces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Delta delta = new Delta();
+            Console.WriteLine("Type a:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type b:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type c:");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            delta.setA(a);
+            delta.setB(b);
+            delta.setC(c);
+
+            Console.WriteLine("This is delta: {0}", delta.MakeDelta());
+        }
+    }
+}
